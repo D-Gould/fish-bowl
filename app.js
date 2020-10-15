74b9af7ba11bd3a2482ad90ee1dad32a23e8a1ca
D-Gould <danagould32@gmail.com>
2020-10-14 20:42:16 -0700

    getting everything up to date

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
 var express = require('express')
+var app = express()
 var http = require('http')
+let server = http.createServer(app)
 var port = process.env.PORT || 3000
 
-var app = express()
-let server = http.createServer(app)
-let Game = require('./game.js')
-let games = []
+app.use(express.urlencoded())
+
+
+const session = require('express-session')
 app.set('view engine', 'pug')
+
+var io = require('socket.io')(server)
+
+let Game = require('./game.js')
+let games = {}
 
 app.get('/', function (req, res) {
   res.render('index')
+
 })
 
 app.get('/room', function (req, res) {
   console.log("adding a new room!")
   let newGame = new Game();
-  games.push(newGame)
-  res.render('game-screen', {code: newGame.code})
+  games[newGame.code] = newGame
+  console.log(games)
+  res.redirect('/room/'+newGame.code)
 })
+
+app.get('/room/:code', function (req, res) {
+   const thisGame = games[req.params.code.toUpperCase()]
+   res.render('game-screen', {code: thisGame.code})
+})
+const isUsernameAvailable = (username, game) => {
+  console.log(game.players)
+  return !game.players.find(player => {
+    player.username === username
+  })
+}
+
+app.post('/room', function (req, res) {
+  const code = req.body.room_code
+  const username = req.body.username
+  const game = games[code]
+  if (!game) {
+    const message = 'There is no room available for that code.'
+    res.redirect('/', {message: message})
+  } else if (!game.open) {
+    res.redirect('/')
+  } else if (!isUsernameAvailable(username, game)) {
+    res.redirect('/')
+  } else {
+    // game.addPlayer(socket, username)
+    res.render('player-screen')
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+  socket.on('disconnect', function() {
+    console.log('A user disconnected')
+  })
+});
 
 server.listen(port, function () {
   console.log(`listening on ${port}`)
-})
+});
 
 
--- game.js
+++ game.js
@@ -3,17 +3,37 @@ const Player = require('./player')
 class Game {
     constructor() {
         this.sockets = {};
-        this.players = {};
-        this.code = "ABCD"
+        this.players = [];
+        this.code = this.setGameCode()
         this.lastUpdateTyime = Date.now()
+        this.open = true
     }
+
+    randLetter() {
+        var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        var letter = letters[Math.floor(Math.random() * letters.length)];
+        return letter
+    }
+    open() {
+        return this.open
+    }
+
+    setOpen(boolean) {
+        this.open = boolean
+    }
+
+    setGameCode() {
+        return (this.randLetter() + this.randLetter() + this.randLetter() + this.randLetter()) 
+    }
+    
     code() {
         return this.code
     }
 
     addPlayer(socket, username) {
-        this.sockets[socket.id] = socket;
-        this.players[socket.id] = new Player(socket.id, username)
+        // this.sockets[socket.id] = socket;
+        // this.players[socket.id] = new Player(socket.id, username)
+        this.players.push(new Player(socket.id, username))
     }
 }
 
